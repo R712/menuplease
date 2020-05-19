0a1f02006e1c558de34a90e0f7297b133c92a9ae
Ahreum Han <R712@users.noreply.github.com>
2020-05-20 03:04:47 +0900

    Update pm.js

--- pm.js
+++ pm.js
@@ -2,6 +2,6 @@ $(function(){
   var deleteBtn = `<button type="button" title="Delete this!">✖</button>`;
   $('.grid li').append(deleteBtn);
   $('.grid li > button').click(function(){
-    $(this).parent('li').fadeOut(fast);
+    $(this).parent('li').fadeOut(200);
   });
 });
